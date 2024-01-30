10806ab27d0134057b4c19bf712da1d7cefe71fe
Bradley Hower <bradleyhower@proton.me>
2024-01-29 17:26:25 -0800

    fix3

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,11 +1,32 @@
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
+    s = self.path
+    url_components = parse.urlsplit(s)
+    query_string_list = parse.parse_qsl(url_components.query)
+    lex = dict(query_string_list)
+
+    if 'capital' in country:
+      url = f'https://restcountries.com/v3.1/capital/'
+      country_resposne = requests.get(url + lex['capital'])
+      data = country_resposne.json()
+      country = data["name"]["common"]
+      message = f'{["capital"]} is the capital of {country}.'
+    elif 'country' in country:
+      url = f'https://restcountries.com/v3.1/name/'
+      capital_resposne = requests.get(url + lex['country'])
+      data = capital_resposne.json()
+      capital = data["capital"][0]
+      message = f'{["country"]} is the capital of {capital}.'
+    else:
+      message = f'Nothing was found.'
+
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write('Hello, world!'.encode('utf-8'))
+    self.wfile.werite(message.encode('utf-8'))
     return
-  #https://restcountries.com/v3.1/name/{name}
