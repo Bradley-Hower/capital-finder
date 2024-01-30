edaf22bdc53ac44c451915b2cefb3c779f63a44f
Bradley Hower <bradleyhower@proton.me>
2024-01-29 17:06:00 -0800

    capital fix2

--- capital-finder/country.py
+++ capital-finder/country.py
@@ -1,1 +1,11 @@
-https://restcountries.com/v3.1/name/{name}
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write('Hello, world!'.encode('utf-8'))
+    return
+  #https://restcountries.com/v3.1/name/{name}
