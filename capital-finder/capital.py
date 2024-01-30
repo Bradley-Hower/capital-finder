2068078e8a73318f074ab2a8a107759edac56471
Bradley Hower <bradleyhower@proton.me>
2024-01-29 17:00:24 -0800

    capital fix

--- capital-finder/capital.py
+++ capital-finder/capital.py
@@ -3,20 +3,21 @@ from urllib import parse
 import requests
 
 class handler(BaseHTTPRequestHandler):
-  
+
   def do_GET(self):
     s = self.path
     url_components = parse.urlsplit(s)
     query_string_list = parse.parse_qsl(url_components.query)
-    country = dict(query_string_list)
+    lex = dict(query_string_list)
 
-    if capital in country:
-      url = f'https://restcountries.com/v3.1/capital/{capital}'
-      data = url.json()
-      capital = data.name.common
-      message = f'{capital} is the capital of {country}.'
+    if 'capital' in country:
+      url = f'https://restcountries.com/v3.1/capital/'
+      country_resposne = requests.get(url + lex['capital'])
+      data = country_resposne.json()
+      country = data["name"]["common"]
+      message = f'{["capital"]} is the capital of {country}.'
     else:
-      message = f'No country was found when searching for {capital}.'
+      message = f'No country was found when searching for {["capital"]}.'
 
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
